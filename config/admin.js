66d3a116bbf439e4b03bcb1cca7e8a066a094a6e
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2022-05-27 13:36:05 +0200

    chore(4.1.12): upgrade apiToken.salt

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '2ebebd1170aec9e66cf900a18ac2ce41'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  }
 });
